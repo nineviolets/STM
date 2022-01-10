c0b8cd8f02cd4d3430ff71ff7f624d37fccf0197
nineviolets <78781058+nineviolets@users.noreply.github.com>
2022-01-10 22:26:37 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -59,9 +59,9 @@ public static Collection<Integer[]>PrimeNumber(){
 }
 
 run the code
-Test case id: ID_5                                                                    Test Designed by: Kanishka 
+Test case id: ID_5                                                                    Test Designed by:  
 Test Priority: low                                                                    Test Designed Date:
-Module Name: Junit Testing                                                            Test Executed by:  Kanishka
+Module Name: Junit Testing                                                            Test Executed by:  
 Test Title: Blackbox testing
 Test Executed Date:
 Description: Test case for problem using Parameterized Testing
